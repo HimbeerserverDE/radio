995aef605777cdf4874c987e0e260c6486ff83c2
HimbeerserverDE <himbeerserverde@gmail.com>
2022-03-25 19:02:49 +0100

    Fix some server bugs

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import random
+import subprocess
 from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
@@ -7,3 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return redirect(url_for('static', filename='radio.html'))
+
+@app.route("/song")
+def song():
+    cmd   = 'ls -1 static/song'
+    proc  = subprocess.run(cmd, shell=True, text=True, stdout=subprocess.PIPE)
+    files = proc.stdout.splitlines()
+    return random.choice(files)
