9ebe1056b17d2b810fcb25e92cc8d8064df5b319
HimbeerserverDE <himbeerserverde@gmail.com>
2022-03-25 17:42:35 +0100

    Hello, static HTML!

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return redirect(url_for('static', filename='radio.html'))
