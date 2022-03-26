a64af31c3faa18adc8f134aa6a8d03c5fe8fb51c
HimbeerserverDE <himbeerserverde@gmail.com>
2022-03-26 10:07:06 +0100

    Handle HTTP API errors on the client side

--- static/radio.js
+++ static/radio.js
@@ -2,9 +2,15 @@ async function play() {
 	let xhr = new XMLHttpRequest();
 	xhr.open("GET", "/song");
 	xhr.onload = _ => {
-		let audio = document.querySelector("#audio");
-		audio.src = "/static/song/" + xhr.responseText;
-		audio.play();
+		if (xhr.status === 200) {
+			let audio = document.querySelector("#audio");
+			audio.src = "/static/song/" + xhr.responseText;
+			audio.play();
+		} else if (xhr.status === 404) {
+			alert("No songs found!");
+		} else {
+			alert("Unknown error!");
+		}
 	};
 	xhr.send();
 }
