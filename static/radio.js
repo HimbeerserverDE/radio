f13d8bf522e8011732acecf6231dbeaa7132de57
HimbeerserverDE <himbeerserverde@gmail.com>
2022-06-06 23:32:03 +0200

    Display current song title / file name

--- static/radio.js
+++ static/radio.js
@@ -6,6 +6,8 @@ async function play() {
 			let audio = document.querySelector("#audio");
 			audio.src = "/static/song/" + xhr.responseText;
 			audio.play();
+
+			document.querySelector("#title").innerText = xhr.responseText;
 		} else if (xhr.status === 404) {
 			alert("No songs found!");
 		} else {
